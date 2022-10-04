1db025f2c2666903044b60569b3a7f57dba54b05
Changbum97 <chb2005@naver.com>
2022-10-04 15:18:53 +0900

    홀수 짝수 문제 2

---
+++ src/CodeUp/p1161.java
@@ -0,0 +1,18 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class p1161 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        System.out.println(print(x) + "+" + print(y) + "=" + print(x + y));
+    }
+    static String print(int x) {
+        if(x % 2 == 0) {
+            return "짝수";
+        }
+        return "홀수";
+    }
+}
