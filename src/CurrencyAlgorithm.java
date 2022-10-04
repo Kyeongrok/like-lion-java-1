c73b014c0746c7c90774889a4519c7b356f77707
Changbum97 <chb2005@naver.com>
2022-10-04 13:46:23 +0900

    commit

--- src/CurrencyAlgorithm.java
+++ src/CurrencyAlgorithm.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Test {
+public class CurrencyAlgorithm {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("금액을 입력하세요 : ");
